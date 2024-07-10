3982b4cb590a67f9ef363062485133db48aef85a
Deepak <deepakworkpc@gmail.com>
2024-07-10 13:54:30 +0530

    total int recursively

---
+++ multiDiArr.js
@@ -0,0 +1,62 @@
+// mind fuck in the beginning but i later realised its pretty straightforward
+// const totalIntegers = (arr, count = 0) => {
+//     if (arr.length == 0) {
+//         return count
+//     } else {
+//         if (arr[0].constructor === Array) {
+//             return totalIntegers(arr[0], count) + totalIntegers(arr.slice(1), 0);
+//         } else {
+//             return totalIntegers(arr.slice(1), count+1)
+//         }
+//     }
+// }
+
+
+// const lengthArr = (arr, count = 0) => {
+//     if (arr.length == 0) {
+//         return count;
+//     } else {
+//         return lengthArr(arr.slice(1), count + 1);
+//     }
+    
+// }
+
+const totalIntegers = (arr, c = 0) => {
+    arr.forEach(element => {
+        if (typeof element === 'number')
+            c++;
+        else
+            c = totalIntegers(element, c);
+    });
+
+    return c;
+}
+
+
+
+let arr = [1, [2, 1, [1, 2 , 2 ,[0]]], 2]
+let nArr = [
+    1,
+    [
+        1, 1, 
+        [
+            1, 11, 3, 
+            [
+                2
+            ]
+        ]
+    ], 
+    [
+        1, 3
+    ],
+    1 
+];
+let nnArr = [
+    1, 
+    [
+        2, 1
+    ], 2
+];
+console.log(totalIntegers(arr))
+console.log(totalIntegers(nArr))
+console.log(totalIntegers(nnArr))
